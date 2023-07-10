91568f994ddb6803aaf627f2ebce9f218020b9cd
DESKTOP-ECAVQVR\PC <des321321@naver.com>
2023-07-10 12:44:21 +0900

    임시

--- src/pages/Chatting.tsx
+++ src/pages/Chatting.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { CheckAuth } from "../utils/authUtils";
 import { useLocation } from 'react-router-dom';
 import { doc, getDoc } from "firebase/firestore";
-import { dbService } from "../firebase";
+import { authService, dbService } from "../firebase";
 import styled from "styled-components";
 
 interface participationsProp {
@@ -30,11 +30,21 @@ interface ChattingDataProp {
 function Chatting() {
     const location = useLocation().pathname;
     const [chattiongData, setChattiongData] = useState<ChattingDataProp>();
-    
+    const [userObj, setUserObj] = useState<any>();
+
     // 로그인 확인
     useEffect(() => {
         CheckAuth('1:1 채팅은 로그인 후 사용 가능합니다.');
     }, [])
+
+    // 유저 정보 가져오기
+    useEffect(() => {
+        authService.onAuthStateChanged((user) => {
+            if (user) {
+                setUserObj(user);
+            }
+        })
+    }, []);
 
     // 채팅창 data get
     useEffect(() => {
@@ -53,7 +63,14 @@ function Chatting() {
 
     return(
         <ChattingBox>
-
+            <ChattingHeader>
+                <OpponentImg src={''}/>
+                <OpponentName>
+                    {userObj &&
+                        userObj.email
+                    }
+                </OpponentName>
+            </ChattingHeader>
         </ChattingBox>
     );
 }
@@ -62,10 +79,22 @@ export default Chatting;
 
 const ChattingBox = styled.div`
     height: 70vh;
-    background-color: #f5f9ff;
-    width: 50%;
+    background-color: #f1f8ff;
+    width: 45%;
     margin: 85px auto 30px auto;
     border: 2px solid #6f9fe7;
-    border-radius: 20px;
-    box-shadow: 1px 1px 3px #6f9fe7;
+    border-radius: 10px;
+    box-shadow: 1px 1px 2px #d4d4d4, -1px -1px 2px #d4d4d4;
+`;
+
+const ChattingHeader = styled.header`
+    display: flex;
+`;
+
+const OpponentImg = styled.img`
+    
+`
+
+const OpponentName = styled.h3`
+
 `;
